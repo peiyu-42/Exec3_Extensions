26ce207052bf3cdd060f7b031ccc5c63ca797f43
ispan <ispan@DESKTOP-PAKSETB>
2022-11-09 10:48:26 +0800

    加入專案檔案。

---
+++ Q1/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q1
+{
+    /* 
+     * Int32.IsOdd, IsEven
+     *   IsOdd()傳回 bool , 表示是否為奇數
+     *   IsEven()傳回 bool , 表示是否為偶數 
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = DateTime.Now.Day;
+            Console.WriteLine(number);
+            Console.WriteLine($"偶數: {number.IsEven()}");
+            Console.WriteLine($"奇數: {number.IsOdd()}");
+        }
+    }
+    public static class MyMath
+    {
+        public static bool IsOdd(this int num)
+        {
+            if (num % 2 == 1)
+            { return true; }                
+
+            return false;
+        }
+        public static bool IsEven(this int num)
+        {
+            if ((num % 2) == 0)
+            { return true; }
+
+            return false ;
+        }
+        
+    }
+}
---
+++ Q2/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Q2
+{
+    /*
+     * String.Left(int length)
+     *   傳回字串的前面幾個字
+     *   若value 是 null or empty, 都傳回 空字串
+     *   若 length <= 0, 傳回空字串
+     *   若value長度比length少, 傳回value
+     */
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string values = "0123456789";
+            int n = 5;
+            Console.WriteLine($"傳回前 {n}個字: {values.Left(n)}");
+            n = 22;
+            Console.WriteLine($"傳回前 {n}個字: {values.Left(n)}");
+        }
+    }
+    public static class MyString
+    {
+        public static string Left(this string value, int length)
+        {
+            string result = string.Empty;
+            if (string.IsNullOrEmpty(value) == true)
+            { return result; }
+            if (length <= 0)
+            { return result; }
+            if (value.Length < length)
+            { return value; }
+
+            return result = value.Substring(0, length);
+        }
+    }
+}
